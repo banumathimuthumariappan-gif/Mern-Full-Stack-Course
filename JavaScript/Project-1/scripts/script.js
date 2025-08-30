602ccd1fed5172132646019554b136d579380694
Banumathi <banumathimuthumariappan691997@gmail.com>
2025-08-30 15:52:22 +0000

    JavaScript

--- JavaScript/Project-1/scripts/script.js
+++ JavaScript/Project-1/scripts/script.js
@@ -1,5 +1,17 @@
+// Login check -- Js -- Critical Script
+// Carousels, chat widgets -- Non Crticial Script
+
 var clickButton = document.getElementById('myButton');
 clickButton.addEventListener("click", function() {
     alert("Hello from external script");
 });
 console.log("External script loaded!");
+
+const element = document.querySelector('h1');
+console.log(element.nodeType);
+console.log(element.firstChild.nodeType); 
+console.log(element.firstChild.nodeName);
+
+const newH2 = document.createElement('h2');
+newH2.textContent = "This is a new h2 element";
+document.body.append(newH2);
