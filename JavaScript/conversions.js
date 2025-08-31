9eb98f05a44207d33feb68137c68ad98e598013f
Banumathi Muthumariappan <banumathimuthumariappan691997@gmail.com>
2025-08-31 21:30:09 +0530

    JavaScript code added

---
+++ JavaScript/conversions.js
@@ -0,0 +1,19 @@
+// 4. Conversions
+
+console.log("\n=== CONVERSIONS ===");
+
+console.log("'5' + 3 =", '5' + 3);        // "53"
+
+console.log("'5' - 3 =", '5' - 3);        // 2
+
+console.log("Number('123') =", Number('123')); // 123
+
+console.log("String(456) =", String(456));     // "456"
+
+console.log("Boolean('') =", Boolean(''));     // false
+
+console.log("Boolean('hello') =", Boolean('hello')); // true
+
+
+
+
